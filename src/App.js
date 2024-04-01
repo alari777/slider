f5ee23546b51713f1d977b9b9aa402f46b4e6de4
alari777 <alari777@gmail.com>
2024-04-01 12:50:15 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import './App.css';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+const images = [
+    {
+        label: 'San Francisco – Oakland Bay Bridge, United States',
+        imgPath:
+            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+    {
+        label: 'Bird',
+        imgPath:
+            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+    {
+        label: 'Bali, Indonesia',
+        imgPath:
+            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+    },
+    {
+        label: 'Goč, Serbia',
+        imgPath:
+            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div style={{display: 'flex', width: '40%'}}>
+      <Swiper
+          spaceBetween={10}
+          slidesPerView={1.5}
+          centeredSlides={true}
+          loop={true}
+          initialSlide={1}
+          speed={100}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          pagination={{ clickable: true }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+      >
+        {images && (images.map((image) => (
+            <SwiperSlide key={image.label}>
+              <img src={image.imgPath} alt={image.label} />
+            </SwiperSlide>
+        )))}
+      </Swiper>
+      </div>
     </div>
   );
 }
